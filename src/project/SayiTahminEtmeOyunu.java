d9d568cf6ca7d3ab067fb83a3c5cc2db57c65c1c
HalUk <haluk...@HalUk-MacBook-Pro.local>
2023-06-24 13:42:10 +0300

    24/06

---
+++ src/project/SayiTahminEtmeOyunu.java
@@ -0,0 +1,16 @@
+package project;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+Sayı tahmin etme oyunu....
+Bilgisayardan rastle(random class kullanılarak) 0-100 arasında bir sayı alıp kullanıcının
+tahminini her defasından büyük-kücük yönlendirerek rastgele sayiyi bulmasını sağlayan code create ediniz .
+*/
+public class SayiTahminEtmeOyunu {
+    public static void main(String[] args) {
+
+    }
+}
+
