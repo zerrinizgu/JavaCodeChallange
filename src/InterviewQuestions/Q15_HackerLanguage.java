8c653f1ecc9ce56bdf403bc7a51757afa0a0d306
HalUk <haluk...@HalUk-MacBook-Pro.local>
2023-07-12 22:57:58 +0300

    last

--- src/InterviewQuestions/Q15_HackerLanguage.java
+++ src/InterviewQuestions/Q15_HackerLanguage.java
@@ -20,11 +20,14 @@ public class Q15_HackerLanguage {
         Scanner input=new Scanner(System.in);
         System.out.println("Agam bi şeyler giresen : ");
         String  str=input.nextLine();
-        hackerDili(str);
-        hackerDili("javaCan javvaNur javaSu javaTar javaNaz javaHan");
+        hackerDili1(str);
+        hackerDili1("javaCan javvaNur javaSu javaTar javaNaz javaHan");
+
+        hackerDili2(str);
+        hackerDili2("javaCan javvaNur javaSu javaTar javaNaz javaHan");
     }//main sonu
 
-    private static void hackerDili(String str) {
+    private static void hackerDili1(String str) {
 
         System.out.println(str.
                 replaceAll("s", "5").
@@ -33,5 +36,22 @@ public class Q15_HackerLanguage {
                 replaceAll("i", "1").
                 replaceAll("o", "0"));
 
+    }
+    private static void hackerDili2(String str) {
+        String [] arr = new String[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            arr[i] = str.substring(i,i+1);
+            System.out.print(arr[i] + " ");
+
+        }
+        System.out.println();
+        for (int j = 0; j < str.length(); j++) {
+            if(arr[j].contains("s")) {arr[j] = "5";}
+            if(arr[j].contains("a")) {arr[j] = "4";}
+            if(arr[j].contains("e")) {arr[j] = "3";}
+            if(arr[j].contains("i")) {arr[j] = "1";}
+            if(arr[j].contains("o")) {arr[j] = "0";}
+            System.out.print(arr[j]);
+        }
     }
 }// Class sonu
---
+++ src/InterviewQuestions/Q21_CountFactors.java
@@ -0,0 +1,16 @@
+package InterviewQuestions;
+
+import java.util.Scanner;
+
+public class Q21_CountFactors {
+      /* Task->
+        Type java code by using while loop,
+       Write a program to count the factors of an integer which is entered by user.
+       Girilen sayının tam bolen sayısını print eden code create ediniz.
+       Factors of 12 = 1, 2, 3, 4, 6, 12 ==> 6
+       Factors of 8  = 1, 2, 4, 8 ==> 4
+*/
+      public static void main(String[] args) {
+
+      }
+}
