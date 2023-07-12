faf69645c1e9a7bc1de93a89a98494c5631757e3
HalUk <haluk...@HalUk-MacBook-Pro.local>
2023-07-12 19:53:34 +0300

    12/07

--- src/InterviewQuestions/Q10_FibonacciListNumber.java
+++ src/InterviewQuestions/Q10_FibonacciListNumber.java
@@ -1,6 +1,7 @@
 package InterviewQuestions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,6 +14,36 @@ public class Q10_FibonacciListNumber {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.print("Agam bi sayi giresen  : ");
+        int adet = input.nextInt();
+
+
+        if (adet <= 0) {
+            System.out.println("Agam gelmiimmm oraya adam gibin bişey giresen :( ");
+        } else if (adet == 1) {
+            list.add(0);
+            System.out.println(list);
+        } else if (adet == 2) {
+            list.add(0);
+            list.add(1);
+            System.out.println(list);
+
+        } else {
+            int sayı1 = 0;
+            int sayı2 = 1;
+            int sayı3 = 0;
+            list.add(0);
+            list.add(1);
+            for (int i = 3; i <= adet; i++) {//0-1-1-2-3-5-8-13-21-34....
+                sayı3 = sayı1 + sayı2;
+                list.add(sayı3);
+                sayı1 = sayı2;
+                sayı2 = sayı3;
+            }
+        }
+        System.out.println(list);
     }
 
 }
--- src/InterviewQuestions/Q12_DifferenceBetweenArray_LargestAndSmallestElements.java
+++ src/InterviewQuestions/Q12_DifferenceBetweenArray_LargestAndSmallestElements.java
@@ -1,17 +1,32 @@
 package InterviewQuestions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q12_DifferenceBetweenArray_LargestAndSmallestElements {
     /*
- Create a function that takes an array and the difference between the largest
- and the smallest numbers.
- Ask user to enter array elements.
+    Create a function that takes an array and the difference between the largest
+    and the smallest numbers.
+    Ask user to enter array elements.
     Girilen  bir array'in en buyuk elemani ile
     en kucuk elemanının  farkını return eden  method create ediniz.
  */
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Agam array boyutunu  giresen  : ");
+        int arrBoyut = input.nextInt();
+        int arr[] = new int[arrBoyut];
+        for (int i = 0; i < arrBoyut; i++) {
+            System.out.println("Agam array'in "+ (i+1)+" elemanını giresen : ");
+            arr[i]=input.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);//arr elemanları NO sıralandı : k-> b
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println(arr[arrBoyut - 1] - arr[0]);
 
     }
 }
